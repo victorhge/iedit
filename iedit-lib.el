48497fef547d37b4567dcd256ab59e5f062ae007
Pierre Rouleau <prouleau001@gmail.com>
2025-09-26 10:37:02 -0400

    Emacs < 24 no longer supported
    Emacs 24 introduced lexical-binding.
    The iedit files now use lexical binding and also use the concept of lexical
    closures that became available in Emacs 24.

--- iedit-lib.el
+++ iedit-lib.el
@@ -2,13 +2,13 @@
 
 ;; Copyright (C) 2010 - 2022 Victor Ren
 
-;; Time-stamp: <2025-10-13 19:12:15 Victor Ren>
+;; Time-stamp: <2025-10-13 19:12:40 Victor Ren>
 ;; Author: Victor Ren <victorhge@gmail.com>
 ;; Keywords: occurrence region simultaneous rectangle refactoring
 ;; Version: 0.9.9.9.9
 ;; X-URL: https://github.com/victorhge/iedit
 ;;        https://www.emacswiki.org/emacs/Iedit
-;; Compatibility: GNU Emacs: 22.x, 23.x, 24.x, 25.x
+;; Compatibility: GNU Emacs: 24.x, 25.x
 
 ;; This file is not part of GNU Emacs, but it is distributed under
 ;; the same terms as GNU Emacs.
--- iedit.el
+++ iedit.el
@@ -2,13 +2,13 @@
 
 ;; Copyright (C) 2010 - 2019, 2020, 2021 Victor Ren
 
-;; Time-stamp: <2025-09-29 18:40:15 Victor Ren>
+;; Time-stamp: <2025-09-29 19:06:58 Victor Ren>
 ;; Author: Victor Ren <victorhge@gmail.com>
 ;; Keywords: occurrence region simultaneous refactoring
 ;; Version: 0.9.9.9.9
 ;; X-URL: https://github.com/victorhge/iedit
 ;;        https://www.emacswiki.org/emacs/Iedit
-;; Compatibility: GNU Emacs: 22.x, 23.x, 24.x, 25.x
+;; Compatibility: GNU Emacs: 24.x, 25.x
 
 ;; This file is not part of GNU Emacs, but it is distributed under
 ;; the same terms as GNU Emacs.
